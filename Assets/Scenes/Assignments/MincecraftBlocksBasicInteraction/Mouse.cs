de6d2e2887fa8c4b84e83573b599058e52df8b10
Valentino Cavaricci <valentinocavaricci@Macbook-Pro-2.local>
2026-03-07 14:37:47 -0800

    minecraft-blocks-and-basic-interaction

---
+++ Assets/Scenes/Assignments/MincecraftBlocksBasicInteraction/Mouse.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public float sensitivity = 200f;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);
+        transform.Rotate(Vector3.right * mouseY);
+    }
+}
